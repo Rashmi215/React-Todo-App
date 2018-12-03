d0150c0669fa5e030f92b7f8a189377621a9a72d
Rashmi Singh <rashmi.singh@newgen.co.in>
2018-12-03 21:23:45 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,105 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Paper, Typography, TextField, Button } from '@material-ui/core';
+import { List, ListItem, ListItemText, ListItemSecondaryAction, IconButton } from '@material-ui/core';
+import { Delete } from '@material-ui/icons';
+import { withStyles } from '@material-ui/core/styles';
+
+
+const styles = {
+  root:{
+    margin: "150px 0px 0px 450px",
+    padding: 20,
+    maxWidth: 400
+  },
+  btn: {
+    margin: "30px 0px 0px 0px"
+  },
+  form: {
+    display: 'flex',
+    alignItem: 'baseline',
+    justifyContent: 'space-evenly'
+  }
+};
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+
+  constructor(props){
+    super(props);
+    this.state = {
+          exercises : [
+            { id:1, title: 'Bench Press'},
+            { id:2, title: 'Deadlift'},
+            { id:3, title: 'Squats'}
+          ],
+           title: ''
+    };
   }
+
+  handleChange = ({ target: {name, value}}) =>{
+    this.setState({ [name]: value});
+  };
+
+  handleCreate = (e) =>{
+    e.preventDefault();
+
+    if(this.state.title){
+      this.setState(({ exercises, title }) =>({
+        exercises: [
+          ...exercises,
+          {
+            title: title,
+            id: Date.now()
+          }
+        ],
+        title: ''
+      }));
+    }
+  };
+
+  handleDelete = (id)=>{
+    this.setState(({ exercises}) =>({
+      exercises: exercises.filter(ex => ex.id !== id)
+    }));
+  };
+
+  render() {
+    const { title, exercises } = this.state;
+    const { classes } = this.props;
+     return (
+       <Paper className={classes.root}>
+           <Typography variant='display1' align='center' color='primary' gutterBottom>
+                Exercises
+           </Typography>
+           <form onSubmit = { this.handleCreate } className={classes.form}>
+              <TextField
+                 name = 'title'
+                 label = 'Exercise'
+                 value = {title}
+                 onChange = {this.handleChange}
+                 margin = 'normal'
+              />
+              <Button type = 'submit' color = 'primary' variant = 'raised' className={classes.btn}>
+                Create
+              </Button>
+           </form>
+
+           <List>
+              {exercises.map(({ id, title }) =>
+                <ListItem key={id}>
+                   <ListItemText primary={title} />
+                   <ListItemSecondaryAction>
+                     <IconButton color='primary' onClick= {()=>this.handleDelete(id)}>
+                          <Delete />
+                     </IconButton>
+                   </ListItemSecondaryAction>
+                </ListItem>
+              )}
+           </List>
+
+       </Paper>
+     );
+   }
+
 }
 
-export default App;
+export default withStyles(styles)(App);
--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { createMuiTheme, MuiThemeProvider } from '@material-ui/core/styles';
+import { orange, blue, pink } from '@material-ui/core/colors';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const theme = createMuiTheme({
+  palette: {
+    primary: blue,
+    secondary: pink,
+    type: 'dark'
+  }
+})
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+ReactDOM.render(
+  <MuiThemeProvider theme= {theme}>
+    <App />
+  </MuiThemeProvider>,
+   document.getElementById('root')
+ );
